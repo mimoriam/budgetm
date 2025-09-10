6e1728c52c5b4cab32e0caef14bd665a6e5c5a67
mimo <mimoriam420@gmail.com>
2025-09-10 15:20:22 +0500

    onboarding preliminary

---
+++ lib/constants/appColors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const splashBackground = Color(0xFF428657);
+  static const fullBlackBackground = Color(0xFF1A1A1A);
+  static const darkGreyBackground = Color(0xFF6B7280);
+  static const lightGreyBackground = Color(0xFF9CA3AF);
+  static const onboardingBackground = Color(0xFFFFFFFF);
+  static const gradientStart = Color(0xFFE5F240);
+  static const gradientEnd = Color(0xFFC6D90F);
+  static const buttonBackground = gradientEnd;
+  static const incomeBackground = Color(0xFFE6F9E8);
+  static const expenseBackground = Color(0xFFFDE8E8);
+}
