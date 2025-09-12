2d23731233ea41341c80b36a2142e17996221a3e
mimo <mimoriam420@gmail.com>
2025-09-12 18:03:49 +0500

    create a goal screen preliminary

---
+++ lib/screens/dashboard/navbar/goals/create_goal/create_goal_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateGoalScreen extends StatefulWidget {
+  const CreateGoalScreen({super.key});
+
+  @override
+  State<CreateGoalScreen> createState() => _CreateGoalScreenState();
+}
+
+class _CreateGoalScreenState extends State<CreateGoalScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
