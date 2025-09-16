b746804bca89243aa68e97749b33a66dd4a40259
mimo <mimoriam420@gmail.com>
2025-09-16 12:52:24 +0500

    export & import data preliminary

---
+++ lib/screens/dashboard/navbar/personal/export_data/export_data_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExportDataScreen extends StatefulWidget {
+  const ExportDataScreen({super.key});
+
+  @override
+  State<ExportDataScreen> createState() => _ExportDataScreenState();
+}
+
+class _ExportDataScreenState extends State<ExportDataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
