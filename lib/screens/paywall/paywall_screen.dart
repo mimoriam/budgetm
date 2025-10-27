e8e5a3e75f74ae285a16b127ccc3596ecab0035d
mimo <mimoriam420@gmail.com>
2025-10-27 14:42:32 +0500

    Paywall screen preliminary

---
+++ lib/screens/paywall/paywall_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaywallScreen extends StatefulWidget {
+  const PaywallScreen({super.key});
+
+  @override
+  State<PaywallScreen> createState() => _PaywallScreenState();
+}
+
+class _PaywallScreenState extends State<PaywallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
