91d92308ff0dfac69500b4880928d458571f3050
mimo <mimoriam420@gmail.com>
2025-11-22 19:41:05 +0500

    a

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 25
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = flutter.versionName
     }
 
